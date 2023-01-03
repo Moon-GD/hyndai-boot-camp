2e39fb07a4d03a863e6a1f9d06f1fad232b6ed6a
RentalHub-MAC23 <rentalhub-mac23@RentalHub-MAC23ui-MacBookPro.local>
2023-01-03 11:12:21 +0900

    add : empty console.log() to flush buffer in show.js

--- 0101/console todo/show.js
+++ 0101/console todo/show.js
@@ -25,22 +25,28 @@ function show(toCount) {
         process.stdout.write(`todo리스트 : 총${todoList.length}건 : `);
         
         for(let i=0;i<todoList.length;i++) {
-            console.log(`'${todoList[i]}'`);
+            process.stdout.write(`'${todoList[i]}'`);
         }
+
+        console.log('')
     }
     else if(toCount === "doing") {
         process.stdout.write(`doing리스트 : 총${doingList.length}건 : `);
         
         for(let i=0;i<doingList.length;i++) {
-            console.log(`'${doingList[i]}'`);
+            process.stdout.write(`'${doingList[i]}'`);
         }
+
+        console.log('')
     }
     else if(toCount === "done") {
         process.stdout.write(`done리스트 : 총${doneList.length}건 : `);
         
         for(let i=0;i<doneList.length;i++) {
-            console.log(`'${doneList[i]}'`);
+            process.stdout.write(`'${doneList[i]}'`);
         }
+
+        console.log('')
     }
 }
 
